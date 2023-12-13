f905b07d84e53e533e07394cb1b8c0848be182db
itsmob <mobster.dev.96@gmail.com>
2023-12-13 15:15:50 -0400

    feat: :sparkles: Add your first Astro island
    Add Preact to your Astro project - Add a <Greeting /> component with the client:load directive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mob-astro-blog.netlify.app/',
+  integrations: [preact()]
 });
